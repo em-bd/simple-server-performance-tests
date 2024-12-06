5a41f691aca4c8e515e852058e2e0030bea65f75
Em Bradley-DeHaan <emmieb@iastate.edu>
2024-12-05 20:15:40 -0600

    some test scripts and test result log

---
+++ performance-test/test3.js
@@ -0,0 +1,75 @@
+const autocannon = require('autocannon')
+const os = require('os')
+const fs = require('fs')
+
+function logToFile(data) {
+    const filePath = 'logged/test_results.json'
+
+    fs.readFile(filePath, (err, fileData) => {
+        let jsonData = [];
+        if (!err && fileData.length > 0) {
+            try {
+                jsonData = JSON.parse(fileData)
+            } catch (parseError) {
+                console.error('Error parsing the existing JSON data:', err)
+            }
+        }
+
+        jsonData.push(data)
+
+        fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), (err) => {
+            if (err) {
+                console.error('Error writing to file', err)
+            }
+        })
+    })
+}
+
+let peakMemory = 0
+
+const memoryMonitor = setInterval(() => {
+    const memoryUsage = process.memoryUsage().rss
+    peakMemory = Math.max(peakMemory, memoryUsage)
+}, 1000)
+
+let peakCPU = 0
+const cpuMonitor = setInterval(() => {
+    const cpus = os.cpus()
+    let totalCPUUsage = 0
+
+    cpus.forEach(cpu => {
+        const total = Object.values(cpu.times).reduce((acc, vall) => acc + vall, 0)
+        const idle = cpu.times.idle
+        totalCPUUsage += (1 - idle / total) * 100
+    })
+
+    const avgCPUUsage = totalCPUUsage / cpus.length
+    peakCPU = Math.max(peakCPU, avgCPUUsage)
+}, 1000)
+
+const instance = autocannon({
+    url: 'http://localhost:3000/my_facilities',
+    connections: 20,
+    duration: 30,
+    pipelining: 1,
+})
+
+instance.on('done', (result) => {
+    clearInterval(memoryMonitor)
+    clearInterval(cpuMonitor)
+
+    const logResult = {
+        'Throughput': result.requests.average + ' req/sec',
+        '95th Percentile Latency': result.latency.p97_5 + 'ms',
+        'Error Rate': ((result.errors / result.requests.total) * 100) + '%',
+        'Peak Memory Usage': (peakMemory / (1024 * 1024)).toFixed(2) + ' MB',
+        'Peak CPU Usage': peakCPU.toFixed(2) + '%',
+        'Duration': '30s',
+    }
+
+    console.log(logResult)
+
+    logToFile(logResult)
+})
+
+autocannon.track(instance)
